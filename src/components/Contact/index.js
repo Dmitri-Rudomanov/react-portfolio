390a8cbf813ed2041771af267a1b53b07defb219
Dmitri-Rudomanov <rudomanovdmitrij@gmail.com>
2022-11-15 13:53:42 +0200

    ready

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,16 +1,39 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
+import emailjs from '@emailjs/browser'
 import './index.scss'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
+  const refForm = useRef()
 
   useEffect(() => {
     setTimeout(() => {
       return setLetterClass('text-animate-hover')
     }, 4000)
   }, [])
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+    emailjs
+      .sendForm(
+        'service_xgwgizi',
+        'template_9e0ih1m',
+        refForm.current,
+        'ESIBz52Dv3exafyEM'
+      )
+      .then(
+        () => {
+          alert('Message succesfully sent')
+          window.location.reload(false)
+        },
+        () => {
+          alert('Failed to send the message,please try again')
+        }
+      )
+  }
 
   return (
     <>
@@ -30,7 +53,7 @@ const Contact = () => {
             questions, don't hesitate to contact me using below form either.
           </p>
           <div className="contact-form">
-            <form>
+            <form ref={refForm} onSubmit={sendEmail}>
               <ul>
                 <li className="half">
                   <input type="text" name="name" placeholder="Name" required />
@@ -59,11 +82,28 @@ const Contact = () => {
                   ></textarea>
                 </li>
                 <li>
-                  <input type="submit" className="flat-button" value="send" />
+                  <input type="submit" className="flat-button" value="SEND" />
                 </li>
               </ul>
             </form>
           </div>
+        </div>
+        <div className="info-map">
+          Dmitrij Rudomanov
+          <br />
+          Ukraine
+          <br />
+          Dnipro
+          <br />
+          <span>rudomanovdmitrij@gmail.com</span>
+        </div>
+        <div className="map-wrap">
+          <MapContainer center={[48.27036, 35.4024]} zoom={9}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[44.96366, 19.61045]}>
+              <Popup>Sloba lives here, come over for a cup of coffee :)</Popup>
+            </Marker>
+          </MapContainer>
         </div>
       </div>
       <Loader type="pacman" />
